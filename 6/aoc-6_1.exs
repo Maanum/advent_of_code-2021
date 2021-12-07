93c603f490ec3839a0d709dd479e8f25b2a4f146
Kristofer Gordon Maanum <kristofer.maanum@gmail.com>
2021-12-06 21:55:00 -0300

    Fix 6.1 copy/paste error

--- 6/aoc-6_1.exs
+++ 6/aoc-6_1.exs
@@ -33,18 +33,6 @@ defmodule MyScript do
     process_day(new_fish, iter - 1)
   end
 
-    def process_day(fish, iter) do
-    IO.puts "Days Left: #{iter}"
-    fish_at_zero = Enum.count(fish, fn x -> x == 0 end)
-    fish_advanced = Stream.reject(fish, fn x -> x == 0 end)
-    |> Enum.map(fn x -> x - 1 end)
-
-    new_fish = [ List.duplicate(6, fish_at_zero) | [ List.duplicate(8, fish_at_zero) | fish_advanced ]]
-    |> List.flatten()
-
-    process_day(new_fish, iter - 1)
-  end
-
 end
 
 MyScript.laternfish
