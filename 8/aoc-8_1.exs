4be8917734ca3594b6d4097a48a5350c8ef62815
Kristofer Gordon Maanum <kristofer.maanum@gmail.com>
2021-12-08 08:56:14 -0300

    Add day 8, part 1 with solution

---
+++ 8/aoc-8_1.exs
@@ -0,0 +1,23 @@
+defmodule MyScript do
+
+  def signal_processer do
+    # GET INPUT
+
+    {:ok, input} = File.read("input.txt")
+    log_data = String.split(input, "\n", trim: true)
+    |> Enum.map(fn x ->
+      [ _, tail ] = String.split(x, "|", trim: true)
+      # { String.split(head, " ", trim: true), String.split(tail, " ", trim: true)}
+      String.split(tail, " ", trim: true)
+      # tail
+    end)
+    |> List.flatten()
+    |> Enum.map(fn x -> String.length(x) end)
+    |> Enum.frequencies()
+
+    IO.puts log_data[2] + log_data[3] + log_data[4] + log_data[7]
+
+  end
+end
+
+MyScript.signal_processer
